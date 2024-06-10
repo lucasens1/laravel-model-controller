aa10803d386bb62e71a27493e798da0b88acb407
lucasens1 <lucasensini1@gmail.com>
2024-06-10 15:09:05 +0200

    correctly printed movies in card from DB

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,20 @@
 @section('content')
     <h1>Movies List :</h1>
     <div class="container">
-        <div class="row">
-            <div class="col-4">
-                @foreach ($moviesList as $movie)
-                    <div class="card">
-                        {{ $movie->id}}
-                        {{ $movie ->title }}
+        <div class="row row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-4">
+            @foreach ($moviesList as $movie)
+                <div class="col-4">
+                    <div class="card h-100">
+                        <img src="{{ $movie->image }}" alt="">
+                        <div class="card-body text-center">
+                            <h4>{{ $movie ->title}}</h4>
+                            <p><b>{{ $movie ->original_title }}</b></p>
+                            <p><i>{{ $movie ->date }}</i></p>
+                            <p><i><b>{{ $movie ->vote }}</b></i></p>
+                        </div>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
