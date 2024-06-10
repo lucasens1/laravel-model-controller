2175fe837379e5b7ab7bb407615f866f230a6b98
lucasens1 <lucasensini1@gmail.com>
2024-06-10 15:19:29 +0200

    Completed, added some extra queries

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -8,7 +8,16 @@ use App\Models\Movie;
 class HomePageController extends Controller
 {
     public function index(){
+        /* Query per tutti */
         $moviesList = Movie::all();
+        /* Query per voto >= 8 */
+        /* $moviesList = Movie::where('vote', '>=', 8)->get(); */
+        /* Query per Nazionalità */
+        /* $moviesList = Movie::where('nationality','american/british')->get(); */
+        /* Query per film in ordine decrescente dal voto */
+        /* $moviesList = Movie::orderByDesc('vote')->get(); */
+        /* Query per film che iniziano per P */
+        /* $moviesList = Movie::where('title','Like','p%')->get(); */
         /* dd($moviesList); */
         return view('home', compact('moviesList'));
     }
